8f4ef013f03531fbe90ce058786bd39a4da95a33
vic <390002333@qq.com>
2016-09-14 17:19:28 +0800

    添加dom4j包

--- blog-api/src/main/java/com/sqb/blog/api/weixin/WeiXinController.java
+++ blog-api/src/main/java/com/sqb/blog/api/weixin/WeiXinController.java
@@ -1,14 +1,24 @@
 package com.sqb.blog.api.weixin;
 
+import com.sqb.blog.api.cmpt.CtrlAspect;
 import com.sqb.blog.util.AppContext;
 import com.sqb.blog.util.DigestUtil;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 微信公众号开发接口
@@ -18,6 +28,7 @@ import java.util.Arrays;
 @Controller
 @RequestMapping("/wx")
 public class WeiXinController {
+    private static final Logger log = LoggerFactory.getLogger(WeiXinController.class);
 
     /**
      * 校验来自微信服务器的请求
@@ -44,6 +55,32 @@ public class WeiXinController {
         //将三个参数字符串拼接成一个字符串进行sha1加密，并与signature对比,如果相同返回echostr
         if (DigestUtil.digest(content.toString(), DigestUtil.SHA1).equals(signature)) {
             response.getWriter().print(echostr);
+        }
+    }
+
+    /**
+     * 处理所有请求
+     *
+     * @throws Exception
+     */
+    @RequestMapping(value = "/weiXinInterface", method = RequestMethod.POST)
+    @ResponseBody
+    public void weiXinInterface(HttpServletRequest request) throws Exception {
+        try (InputStream inputStream = request.getInputStream()) {
+            //读取输入流
+            SAXReader reader = new SAXReader();
+            Document document = reader.read(inputStream);
+            // 得到xml根元素
+            Element root = document.getRootElement();
+            // 得到根元素的所有子节点
+            //noinspection unchecked
+            List<Element> elementList = root.elements();
+            for (Element e : elementList) {
+                log.info("name={},text={}", e.getName(), e.getText());
+            }
+            // 遍历所有子节点
+        } catch (Exception e) {
+            log.error("", e);
         }
     }
 }
